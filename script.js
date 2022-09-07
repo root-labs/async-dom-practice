4eca0972604d03a86c58fd053225ea055ca5959b
ManishPoduval <manishpoduval@hotmail.com>
2022-09-07 11:47:21 +0530

    setup

---
+++ script.js
@@ -0,0 +1,22 @@
+let startBtn = document.querySelector('#start')
+let stopBtn = document.querySelector('#stop')
+let timeDOM = document.querySelector('#timer')
+
+
+// STEP 3: Create an object from the timer class
+
+function updateTime(){
+   // update the time DOM here
+   // console.log('Here')
+    
+}
+
+
+startBtn.addEventListener('click', () => {
+    // Call the timer start method
+})
+
+
+stopBtn.addEventListener('click', () => {
+    // Call the timer stop method
+})
---
+++ timer.js
@@ -0,0 +1,20 @@
+// add properties and methods for your timer here
+class Timer {
+    //STEP 0: Add necessary properties for your timer
+    constructor(){
+        this.time = 0;
+        this.intervalID = 0;
+    }
+
+    // STEP1: Create a start method to start the timer
+    start(callback){
+           
+    }
+
+
+    // STEP2: Create a stop method to stop the timer
+    stop(){
+       
+    }
+
+}
