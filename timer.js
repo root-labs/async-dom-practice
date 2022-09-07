3a27bbf879eb279ce8cba03c2684de08ab351745
arshmohd <irfan.arshad4@gmail.com>
2022-09-07 12:52:56 +0530

    100% solution done

--- timer.js
+++ timer.js
@@ -1,20 +1,21 @@
 // add properties and methods for your timer here
 class Timer {
-    //STEP 0: Add necessary properties for your timer
-    constructor(){
-        this.time = 0;
-        this.intervalID = 0;
-    }
+  //STEP 0: Add necessary properties for your timer
+  constructor() {
+    this.time = 0;
+    this.intervalID = 0;
+  }
 
-    // STEP1: Create a start method to start the timer
-    start(callback){
-           
-    }
+  // STEP1: Create a start method to start the timer
+  start(callback) {
+    this.intervalID = setInterval(() => {
+      this.time = this.time + 1;
+      updateTime();
+    }, 1000);
+  }
 
-
-    // STEP2: Create a stop method to stop the timer
-    stop(){
-       
-    }
-
+  // STEP2: Create a stop method to stop the timer
+  stop() {
+    clearInterval(this.intervalID);
+  }
 }
